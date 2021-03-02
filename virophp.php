1a5d0997ef7986d20f290eb01c3119c76640781a
Mari-aaa <Mari-aaa@users.noreply.github.com>
2021-03-02 09:55:22 +0100

    First Commit

---
+++ virophp.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Hello</title>
+<style type="text/css">
+<link href="css/bootstrap-4.4.1.css" rel="stylesheet">
+	</style>
+
+</head>
+
+<body>
+	
+</body>
+</html>
+<?php phpinfo(); ?>
+
